a4f6516876f62d7f0ec7793f15290899fbb9949c
guzelyaa <abitovaguzelyakilichbekovna@gmail.com>
2022-08-01 20:47:49 +0600

    dz6

--- src/com/guzelya/Main.java
+++ src/com/guzelya/Main.java
@@ -1,6 +1,10 @@
 package com.guzelya;
 
+import java.sql.Array;
+import java.util.Arrays;
+
 public class Main {
+    public static  String[] array = new String[3];
 
     public static void main(String[] args) {
 
@@ -13,6 +17,20 @@ public class Main {
         System.out.println(boss.getHealth());
         System.out.println(boss.getDamage());
         System.out.println(boss.getTypeOfDefence());
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(hero1.getHealth());
+        }
+
     }
 
+    public String[] createHeroes(){
+        Hero hero1 = new Hero(260, 20, "kinetic");
+        Hero hero2 = new Hero(250,25);
+        Hero hero3 = new Hero(270,30,"physical");
+        array[0] = String.valueOf(hero1);
+        array[1] = String.valueOf(hero2);
+        array[2] = String.valueOf(hero3);
+        return array;
+    }
 }
